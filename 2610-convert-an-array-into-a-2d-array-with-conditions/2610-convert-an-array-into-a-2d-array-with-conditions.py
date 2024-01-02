b8716a1f9700efa19ca52b0bdd182fc338741f72
Ojas Doshi <mr.doshi.ojas@gmail.com>
2024-01-02 10:31:25 +0530

    Time: 72 ms (6.46%), Space: 17.5 MB (5.17%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        nums.sort(reverse=True) 
+        result = []
+
+        while nums:
+            row = [nums.pop(0)] 
+            i = 0
+            while i < len(nums):
+                if nums[i] not in row: 
+                    row.append(nums.pop(i))
+                else:
+                    i += 1
+            result.append(row)
+
+        return result
