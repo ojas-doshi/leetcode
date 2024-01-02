a2162a3c609697994a8a3df768fd0881ca1fad6f
Ojas Doshi <mr.doshi.ojas@gmail.com>
2024-01-02 12:12:41 +0530

    Time: 48 ms (27.70%), Space: 14.1 MB (30.84%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def canVisitAllRooms(self, rooms):
+        """
+        :type rooms: List[List[int]]
+        :rtype: bool
+        """
+        n = len(rooms)
+        visited = set()
+
+        def dfs(room):
+            visited.add(room)
+            for key in rooms[room]:
+                if key not in visited:
+                    dfs(key)
+
+        dfs(0)
+        return len(visited) == n
