a163774b09cbba0498b80c3dcb0177b6b323dbb1
Ojas Doshi <mr.doshi.ojas@gmail.com>
2024-01-02 18:18:45 +0530

    Time: 20 ms (34.26%), Space: 13.3 MB (52.78%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def numTilings(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+        if n == 1:
+            return 1
+
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD
+
+        return dp[n]
