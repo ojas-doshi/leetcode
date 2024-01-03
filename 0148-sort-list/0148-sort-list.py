fc31896fe88664a0dcc27bfa40cf22f52b00386c
Ojas Doshi <mr.doshi.ojas@gmail.com>
2024-01-04 00:19:26 +0530

    Time: 280 ms (86.22%), Space: 33.4 MB (90.68%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    
+    def merge(self,left, right):
+        dummy = ListNode()
+        current = dummy
+
+        while left and right:
+            if left.val < right.val:
+                current.next = left
+                left = left.next
+            else:
+                current.next = right
+                right = right.next
+            current = current.next
+
+        current.next = left or right
+
+        return dummy.next
+
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        mid = slow.next
+        slow.next = None
+
+        left_sorted = self.sortList(head)
+        right_sorted = self.sortList(mid)
+
+        return self.merge(left_sorted, right_sorted)
+        
