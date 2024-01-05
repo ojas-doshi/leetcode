8b495274745e19743a38899b20f3c68b8950c4f9
Ojas Doshi <mr.doshi.ojas@gmail.com>
2024-01-05 18:13:56 +0530

    Time: 0 ms (100.00%), Space: 2.3 MB (13.53%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.rs
@@ -0,0 +1,28 @@
+impl Solution {
+    pub fn roman_to_int(s: String) -> i32 {
+        let mut result = 0;
+        let mut prev_value = 0;
+
+        for c in s.chars().rev() {
+            let current_value = match c {
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+                _ => 0, // Handle invalid characters
+            };
+
+            if current_value < prev_value {
+                result -= current_value;
+            } else {
+                result += current_value;
+            }
+            prev_value = current_value;
+        }
+
+        result
+    }
+}
