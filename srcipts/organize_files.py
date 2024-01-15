ed4afb65f1d4d1536956e13ca9f4911c88e570e2
Ojas Doshi <ojas.doshi@srivisifai.in>
2024-01-15 12:12:25 +0530

    updated script for organizing solutions

--- srcipts/organize_files.py
+++ srcipts/organize_files.py
@@ -7,6 +7,7 @@ readme_file = 'README.md'
 difficulty_pattern = r"</h2><h3>(\w+)</h3><hr>"  # Assuming the difficulty level is mentioned in the README.md
 
 src_default_path = 'src'
+difficulty_levels = ['easy','medium','hard']
 
 # Function to extract difficulty level from README.md
 def extract_difficulty_level(file_path):
@@ -18,16 +19,24 @@ def extract_difficulty_level(file_path):
         else:
             return None
 
+def check_in_previous_solutions(root_folder,folder_name):
+    for level in difficulty_levels:
+        if os.path.exists(os.path.join(root_folder,src_default_path,level,folder_name)):
+            return True,level
+    return False,None
+
+
 # Function to move folders to the respective difficulty level folder
 def organize_folders_by_difficulty(root_folder):
     for folder_name in os.listdir(root_folder):
         folder_path = os.path.join(root_folder, folder_name)
         if os.path.isdir(folder_path):
             readme_path = os.path.join(folder_path, readme_file)
-            if os.path.exists(readme_path):
-                difficulty_level = extract_difficulty_level(readme_path)
+            exists_in_previous, difficulty_level =  check_in_previous_solutions(root_folder, folder_name)
+            if os.path.exists(readme_path) or exists_in_previous:
+                difficulty_level = difficulty_level or extract_difficulty_level(readme_path)
                 if difficulty_level:
-                    difficulty_folder = os.path.join(root_folder,"src", difficulty_level)
+                    difficulty_folder = os.path.join(root_folder,src_default_path, difficulty_level)
                     difficulty_folder = difficulty_folder.lower()
                     if not os.path.exists(difficulty_folder):
                         os.makedirs(difficulty_folder)
