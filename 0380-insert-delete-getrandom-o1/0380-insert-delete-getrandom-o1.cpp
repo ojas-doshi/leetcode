e907f3ca9b1282bbc21a2eff60aee6878ba15b13
Ojas Doshi <mr.doshi.ojas@gmail.com>
2024-01-16 20:16:35 +0530

    Time: 182 ms (44.16%), Space: 97.4 MB (73.46%) - LeetHub

---
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -0,0 +1,49 @@
+class RandomizedSet {
+    
+private:
+    std::unordered_map<int, int> valToIndex;
+    std::vector<int> values;
+    
+public:
+    RandomizedSet() {
+        std::srand(std::time(nullptr)); // Seed for random number generation
+    }
+
+    bool insert(int val) {
+        if (valToIndex.find(val) != valToIndex.end()) {
+            return false;
+        }
+
+        values.push_back(val);
+        valToIndex[val] = values.size() - 1;
+        return true;
+    }
+
+    bool remove(int val) {
+        if (valToIndex.find(val) == valToIndex.end()) {
+            return false;
+        }
+
+        int index = valToIndex[val];
+        int lastVal = values.back();
+        values[index] = lastVal;
+        valToIndex[lastVal] = index;
+
+        values.pop_back();
+        valToIndex.erase(val);
+        return true;
+    }
+
+    int getRandom() {
+        int randomIndex = std::rand() % values.size();
+        return values[randomIndex];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
