e09e2725a192b36d472a3a498c1b457194ed6164
Ojas Doshi <mr.doshi.ojas@gmail.com>
2024-02-10 12:15:27 +0530

    Time: 78 ms (77.03%), Space: 16.5 MB (72.69%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -3,19 +3,18 @@ class Solution:
         n = len(s)
         count = 0
 
-        for i in range(n):
-            # Counting palindromic substrings with odd length
-            left, right = i, i
+        # Function to expand around center and count palindromic substrings
+        def expand_around_center(left, right):
+            nonlocal count
             while left >= 0 and right < n and s[left] == s[right]:
                 count += 1
                 left -= 1
                 right += 1
 
-            # Counting palindromic substrings with even length
-            left, right = i, i + 1
-            while left >= 0 and right < n and s[left] == s[right]:
-                count += 1
-                left -= 1
-                right += 1
+        for i in range(n):
+            # For odd length palindromes
+            expand_around_center(i, i)
+            # For even length palindromes
+            expand_around_center(i, i + 1)
 
         return count
